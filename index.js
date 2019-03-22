b1012bc34da877fa5eb75ab3f5d5d9faa22d7187
Marya Doery <mdoery@gmail.com>
2019-03-22 15:59:59 -0400

    Processes a list of words and removes "plurals" (words that end in a single "s").

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict"
+const fs = require('fs');
+
+/**
+ * Returns a list of words that do not end in a single "s".
+ * So for example, if the list contains "TESS", that will be returned.
+ * But if the list contains "NESTS" that will not be returned.
+ */
+var removePlurals = function(words) {
+	var result = words.filter(function(w) {
+		var lower = w.toLowerCase();
+		if (lower.endsWith('s')) {
+			var l1 = lower.length - 1;
+			return lower[l1].toLowerCase() !== 's';
+		} else {
+			return true;
+		}
+	});
+	return result;
+}
+
+/**
+ * Given an input file name, assumed to contain a list of words separated by newlines,
+ * returns the words as an array.
+
+ */
+var getWordsAsArray = function(filename) {
+	return new Promise(function(resolve, reject) {
+		var words = [];
+		fs.readFile(filename, 'utf8', function(err, content) {
+			if (err) {
+				console.log(err);
+				reject(err);
+			} else {
+				words = content.split(/\r?\n/);
+				resolve(words);
+			}
+		});
+	});
+}
+
+var promise = getWordsAsArray("./data/somewords.txt");
+promise.then(function(value) {
+	var noPlurals = removePlurals(value);
+	noPlurals.forEach(function(w) {
+		console.log(w.toUpperCase());
+	});
+}).catch(function(err) {
+	console.log("There was an error");
+	console.log(err);
+})
+
+if (typeof module !== 'undefined' && module.exports !== null) {
+	exports.getWordsAsArray = getWordsAsArray;
+}
---
+++ test/test.js
@@ -0,0 +1,20 @@
+"use strict"
+var assert = require('assert');
+var wordlistProcessor = require('../index');
+var getWordsAsArray = wordlistProcessor.getWordsAsArray;
+
+/*****************\
+| Tests of isFile |
+\*****************/
+describe('test', function() {
+  describe('#getWordsAsArray()', function() {
+    it('should return list of words', function() {
+      return getWordsAsArray('test/data.txt').then(function(value) {
+        assert.deepEqual(value, ['PLOT','SAMPLE','DATA','INFINITE','BEST','BESTIES','LEAST','TESTS','TEST','EXAMPLE','EXAMPLES'],
+        'got ' + JSON.stringify(value));
+      }).catch(function(err) {
+        throw new Error('Failed returning list of words');
+      });
+    });
+  });
+});
